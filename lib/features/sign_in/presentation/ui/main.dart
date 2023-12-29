45c08d0304b9ec7816d09b54d2ebced1eda547a7
JosephKorel <jcjosecarlos03@gmail.com>
2023-12-29 16:30:59 -0300

    feat: make UI of sign  in screen

--- lib/features/sign_in/presentation/ui/main.dart
+++ lib/features/sign_in/presentation/ui/main.dart
@@ -1,8 +1,10 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_sign_in/google_sign_in.dart';
-import 'package:mumag/features/connect/presentation/providers/connect.dart';
+import 'package:mumag/common/theme/utils.dart';
+import 'package:phosphor_flutter/phosphor_flutter.dart';
 
 class SignInView extends ConsumerWidget {
   const SignInView({super.key});
@@ -22,22 +24,62 @@ class SignInView extends ConsumerWidget {
       return FirebaseAuth.instance.signInWithCredential(credential);
     }
 
-    void connectToSpotify() {
-      ref.read(handleConnectionProvider.notifier).connect();
-    }
-
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        systemOverlayStyle: SystemUiOverlayStyle.light,
+        backgroundColor: Colors.transparent,
+      ),
+      extendBodyBehindAppBar: true,
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          const Text('Sign In Page'),
-          ElevatedButton(
-            onPressed: signInWithGoogle,
-            child: const Text('Sign In With Google'),
-          ),
-          ElevatedButton(
-            onPressed: connectToSpotify,
-            child: const Text('Connect'),
+          Image.asset('assets/sign-in-img.png'),
+          Expanded(
+            child: DecoratedBox(
+              decoration: const BoxDecoration(
+                color: Color(0xFF1d1615),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 32),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    OutlinedButton.icon(
+                      onPressed: signInWithGoogle,
+                      style: OutlinedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        foregroundColor: Colors.white,
+                        textStyle: context.titleMedium,
+                        side: const BorderSide(width: 1.6, color: Colors.white),
+                        padding: const EdgeInsets.all(12),
+                      ),
+                      icon: const Icon(PhosphorIconsBold.googleLogo),
+                      label: const Text('Sign In With Google'),
+                    ),
+                    const Divider(
+                      height: 80,
+                    ),
+                    OutlinedButton.icon(
+                      onPressed: () {},
+                      style: OutlinedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        foregroundColor: Colors.white,
+                        textStyle: context.titleMedium,
+                        side: const BorderSide(width: 1.6, color: Colors.white),
+                        padding: const EdgeInsets.all(12),
+                      ),
+                      icon: const Icon(PhosphorIconsBold.appleLogo),
+                      label: const Text('Sign In With Apple ID'),
+                    ),
+                  ],
+                ),
+              ),
+            ),
           ),
         ],
       ),
